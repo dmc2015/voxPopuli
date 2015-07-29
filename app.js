4ea3802761c1aab47b0a9830d82a0cf12942d333
Don McLamb <mclamb.donald@gmail.com>
2015-07-29 17:09:47 -0400

    added comments succesfully and comments controller. need to fix some bugs related to the upvote/downvote that i added outtside fo the tuturoial

--- app.js
+++ app.js
@@ -10,59 +10,87 @@ app.config([
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
+		})//;
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 		$urlRouterProvider.otherwise('home');
-	}]);
+}]);
 
-	app.factory('posts', [function(){
-		//the body of a service
-		var postobject = {
-			posts: [
-				{title: 'post 1', upvotes:2, downvotes:1},
-				{title: 'post 2', upvotes:1, downvotes:1},
-				{title: 'post 3', upvotes:5, downvotes:1},
-				{title: 'post 4', upvotes:4, downvotes:1},
-				{title: 'post 5', upvotes:11, downvotes:1}
-			]
+app.factory('posts', [function(){
+	//the body of a service
+	var postobject = {
+		posts: [
+
+		]
+		// posts: [
+		// 	{title: 'post 1', upvotes:2, downvotes:1, comments: ''},
+		// 	{title: 'post 2', upvotes:1, downvotes:1},
+		// 	{title: 'post 3', upvotes:5, downvotes:1},
+		// 	{title: 'post 4', upvotes:4, downvotes:1},
+		// 	{title: 'post 5', upvotes:11, downvotes:1}
+		// ]
+	};
+	return postobject;
+}]);
+
+app.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts){
+		$scope.test = 'Hello world!';
+		$scope.posts = posts.posts;/*[
+			{title: 'post 1', upvotes:2},
+			{title: 'post 2', upvotes:1},
+			{title: 'post 3', upvotes:5},
+			{title: 'post 4', upvotes:4},
+			{title: 'post 5', upvotes:11}
+			];*/
+
+			$scope.addPost = function(){
+				if(!$scope.title || $scope.title === "") {return;}
+				$scope.posts.push({
+					title: $scope.title,
+					link: $scope.link,
+					upvotes:0,
+					comments: [
+						// {author: 'Dave', body: 'Well said', upvotes: 0},
+						// {author: 'Bill Bob', body: 'You lie', upvotes: 1}
+					]
+				});
+				$scope.title="";
+				$scope.link="";
+			};
+
+
+			$scope.incrementUpvotes = function(post){
+				post.upvotes +=1;
+			};
+
+			$scope.incrementDownvotes = function(post){
+				post.downvotes +=1;
+			};
+}])//;
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+			if ($scope.body === '') {return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0,
+				downvotes: 0
+			});
+			$scope.body = '';
 		};
-		return postobject;
-	}]);
-
-	app.controller('MainCtrl', [
-		'$scope',
-		'posts',
-		function($scope, posts){
-			$scope.test = 'Hello world!';
 
 
-			$scope.posts = posts.posts;/*[
-				{title: 'post 1', upvotes:2},
-				{title: 'post 2', upvotes:1},
-				{title: 'post 3', upvotes:5},
-				{title: 'post 4', upvotes:4},
-				{title: 'post 5', upvotes:11}
-				];*/
-
-				$scope.addPost = function(){
-					if(!$scope.title || $scope.title === "") {return;}
-					$scope.posts.push({
-						title: $scope.title,
-						link: $scope.link,
-						upvotes:0
-					});
-					$scope.title="";
-					$scope.link="";
-
-				};
-
-				$scope.incrementUpvotes = function(post){
-					post.upvotes +=1;
-
-				};
-
-				$scope.incrementDownvotes = function(post){
-					post.downvotes +=1;
-				};
-
-
-			}]);
+}]);
