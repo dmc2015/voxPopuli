a08c610dfc28f2e6a8e39e5ae8aee25e1fe075d9
Don McLamb <mclamb.donald@gmail.com>
2015-07-30 16:18:24 -0400

    wrote post route for node

--- VoxPopuli/routes/index.js
+++ VoxPopuli/routes/index.js
@@ -1,5 +1,20 @@
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if(err)}{ return next(err); }
+    res.json(posts);
+  });
+});
+
+
 var express = require('express');
 var router = express.Router();
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
