37c311ecd588f81f5b8400682b769f6f31e64a9f
Don McLamb <mclamb.donald@gmail.com>
2015-07-30 16:57:30 -0400

    added middleware function to find post id, set up route to view single post

--- VoxPopuli/routes/index.js
+++ VoxPopuli/routes/index.js
@@ -1,28 +1,44 @@
+var express = require('express');
+var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+//GETS ALL POSTS
 router.get('/posts', function(req, res, next){
   Post.find(function(err, posts){
-    if(err)}{ return next(err); }
+    if(err){ return next(err); }
     res.json(posts);
   });
 });
 
+//POSTS A NEW post
 router.post('/posts', function(req, res, next){
   var post = new Post(req.body);
-
   post.save(function(err, post){
     if(err){ return next(err); }
-
     res.json(post);
   });
+});
 
+//FINDS THE ID OF A GIVEN POST - THIS IS A MIDDLEWARE FUNCTION
+router.param('post', function(req, res, next, id){
+  var query = POst.findById(id);
+
+  query.exec(function (err, post){
+      if (err) {return next(err); }
+      if (!post) { return next(new Error('can\'t find post'));}
+      req.post = post;
+      return next();
+  });
 });
 
 
-var express = require('express');
-var router = express.Router();
+//UTILIZES THE ABOVE ROUTE TO FIND A GIVEN POST BASED ON ITS ID
+router.get('/posts/:post', function(req, res){
+  res.json(req.post);
+});
+
 
 
 
