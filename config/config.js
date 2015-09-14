0c094d2ad2043ee700cb4cf2f57e8fb5bae4c31c
Don McLamb <mclamb.donald@gmail.com>
2015-09-14 17:44:36 -0400

    first deploy is failing, moved config to root

---
+++ config/config.js
@@ -0,0 +1,15 @@
+development: {
+  db: 'mongodb: //localhost/voxpopuli',
+  root: '.',
+  app: {
+    name: 'VoxPopuli',
+  }
+}
+
+production: {
+  db: 'mongodb: mongodb://cyberdesigns.io:don555@ds061777.mongolab.com:61777/voxpopuli',
+  root: '.',
+  app: {
+    name: 'VoxPopuli',
+  }
+}
