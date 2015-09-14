3ddb99664b34a83dc92ff676d3529e9b567c7d94
Don McLamb <mclamb.donald@gmail.com>
2015-09-14 17:35:37 -0400

    added production config for deployment

--- VoxPopuli/config/config.js
+++ VoxPopuli/config/config.js
@@ -5,3 +5,11 @@ development: {
     name: 'VoxPopuli',
   }
 }
+
+production: {
+  db: 'mongodb: mongodb://cyberdesigns.io:don555@ds061777.mongolab.com:61777/voxpopuli',
+  root: '.',
+  app: {
+    name: 'VoxPopuli',
+  }
+}
